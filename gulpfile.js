0642bbb2eb095529e4c8e89f8953afb0aaaa77a1
Daniel Silva <daniel5055@gmail.com>
2016-07-11 15:28:31 -0300

    Initial structure

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+var gulp = require('gulp'),
+    perfTool = require('devbridge-perf-tool'),
+    SITE_PAGES = [
+      '/',
+      '/seguro-auto',
+      '/seguro-auto/seguro-uber',
+      '/seguro-auto/carros-antigos',
+      '/seguro-auto/seguro-taxi',
+      '/seguro-moto',
+      '/seguro-residencial',
+      '/seguro-de-acidentes-pessoais',
+      '/seguro-viagem',
+      '/seguro-celular',
+      '/seguro-camera-fotografica',
+      '/seguro-filmadora',
+      '/seguro-notebook',
+      '/seguro-tablet',
+      '/porto-seguro-conecta',
+      '/siggo-sascar',
+      '/consorcio-carro',
+      '/consorcio-imobiliario',
+      '/blog',
+      '/blog/transporte-sp-tecnologia/',
+      '/quem-somos',
+      '/central-de-ajuda'
+    ];
+
+gulp.task('perf-tool', function () {
+  perfTool.performance({
+    siteURL: 'http://www.bidu.com.br',
+    sitePages: SITE_PAGES
+  });
+});
+
+gulp.task('default', ['perf-tool']);
